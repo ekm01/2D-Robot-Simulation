1c17e3641e72afb14da0714f9b2f258627db9291
binotto <ekm7p8j@gmail.com>
2023-11-15 15:03:50 +0100

    fix main

--- src/robot.rs
+++ src/robot.rs
@@ -12,7 +12,38 @@ pub mod robot {
     const DEF_THINNING: f32 = 0.02;
     const DEF_HEIGHT: f32 = 0.4;
 
-    pub fn generate_vertices(center_x: f32, center_y: f32) -> (Vec<Vertex>, Vec<u16>, Vec<f32>) {
+    pub struct Chain {
+        // link and joint
+        pub vertices: Vec<Vertex>,
+        pub indices: Vec<u32>,
+        pub middle: Vertex,
+        pub vertex_buffer: glium::VertexBuffer<Vertex>,
+        pub index_buffer: glium::IndexBuffer<u32>,
+        pub program: glium::program::Program,
+    }
+
+    pub fn generate_joint(
+        center_x: f32,
+        center_y: f32,
+        r: &str,
+        g: &str,
+        b: &str,
+        disp: &glium::Display<WindowSurface>,
+    ) -> Chain {
+        let (vertices, indices, middle) = generate_vertices(center_x, center_y);
+        let (vertex_buffer, index_buffer) = generate_vertex_index_buffer(disp, &vertices, &indices);
+        let program = generate_program(r, g, b, disp);
+        Chain {
+            vertices,
+            indices,
+            middle,
+            vertex_buffer,
+            index_buffer,
+            program,
+        }
+    }
+
+    fn generate_vertices(center_x: f32, center_y: f32) -> (Vec<Vertex>, Vec<u32>, Vertex) {
         let mut vertices = vec![
             Vertex {
                 position: [center_x + DEF_RADIUS - DEF_THINNING, center_y + DEF_HEIGHT], // top right
@@ -24,7 +55,7 @@ pub mod robot {
                 position: [center_x - DEF_RADIUS + DEF_THINNING, center_y + DEF_HEIGHT], // top left
             },
         ];
-        let mut indices: Vec<u16> = (0..=3).collect();
+        let mut indices: Vec<u32> = (0..=3).collect();
 
         // generate vertices and indices for circle
         let circle_segments = 100;
@@ -35,15 +66,29 @@ pub mod robot {
             vertices.push(Vertex {
                 position: [x + center_x, y + center_y],
             });
-            indices.push(i as u16);
+            indices.push(i as u32);
         }
 
-        let middle = vec![center_x, center_y + DEF_HEIGHT];
+        let middle = Vertex {
+            position: [center_x, center_y + DEF_HEIGHT],
+        };
 
         (vertices, indices, middle)
     }
 
-    pub fn generate_program(
+    fn generate_vertex_index_buffer(
+        disp: &glium::Display<WindowSurface>,
+        vertexbuf: &Vec<Vertex>,
+        indexbuf: &Vec<u32>,
+    ) -> (glium::VertexBuffer<Vertex>, glium::IndexBuffer<u32>) {
+        (
+            glium::VertexBuffer::new(disp, vertexbuf).unwrap(),
+            glium::IndexBuffer::new(disp, glium::index::PrimitiveType::TriangleFan, indexbuf)
+                .unwrap(),
+        )
+    }
+
+    fn generate_program(
         r: &str,
         g: &str,
         b: &str,
